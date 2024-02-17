3b974b279548ffd648d00ba5e9efe65c3dfa175a
hghgknh <filip.momchilov@gmail.com>
2024-02-17 18:07:14 +0200

    Add project files.

---
+++ project/DbManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public class DbManager
+    {
+        private static DbManager instance = null;
+        private string connectionString;
+        private SqlConnection connection;
+        public DbManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DbManager();
+                }
+                return instance;
+            }
+        }
+
+        public DbManager()
+        {
+            try
+            {
+                connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"" + Path.GetFullPath(Directory.GetCurrentDirectory() + "\\..\\..\\Database1.mdf") + "\";Integrated Security=True";
+                //connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Database1_OLD;Persist Security Info=True;User ID=nmb; Password=nos";
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void Dispose()
+        {
+            connection.Close();
+            instance = null;
+        }
+
+
+
+
+
+        public DataTable SelectMenu()
+        {
+            SqlCommand cmd = new SqlCommand("Select * FROM MenuItem", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            adapter.Dispose();
+            return table;
+        }
+    }
+}
