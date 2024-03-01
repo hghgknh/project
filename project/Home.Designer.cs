165ecc800826b49fbe0278368a20bdbe8886e03d
hghgknh <filip.momchilov@gmail.com>
2024-03-01 21:53:40 +0200

    ?

--- project/Home.Designer.cs
+++ project/Home.Designer.cs
@@ -39,9 +39,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(184, 83);
+            this.label1.Location = new System.Drawing.Point(138, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(437, 69);
+            this.label1.Size = new System.Drawing.Size(352, 55);
             this.label1.TabIndex = 0;
             this.label1.Text = "Добре Дошли!";
             // 
@@ -49,24 +50,27 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(258, 178);
+            this.label2.Location = new System.Drawing.Point(194, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(286, 25);
+            this.label2.Size = new System.Drawing.Size(229, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Изберете място за доставка";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(218, 270);
+            this.textBox1.Location = new System.Drawing.Point(163, 204);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(362, 22);
+            this.textBox1.Size = new System.Drawing.Size(272, 20);
             this.textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(293, 316);
+            this.button1.Location = new System.Drawing.Point(221, 238);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 30);
+            this.button1.Size = new System.Drawing.Size(157, 24);
             this.button1.TabIndex = 3;
             this.button1.Text = "Потвърди";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,14 +78,15 @@
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Home";
             this.Text = "Home";
             this.ResumeLayout(false);
